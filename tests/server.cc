6eae2ede0ec928baa8b0732676511f3460a60a72
Olle K <olle.kvarnstroem@gmail.com>
2014-03-27 20:40:49 +0100

    minor change to test cases

--- tests/server.cc
+++ tests/server.cc
@@ -32,8 +32,8 @@ TEST(Connect, ipv6) {
 
 TEST(Connect, ipv4_copycontructor) {
   TCPSocket server_constructor(IPV4);
+  server_constructor.reuseAddr(1);
   TCPSocket server(server_constructor);
-  server.reuseAddr(1);
   ASSERT_EQ(server.bind(1337), 0);
   ASSERT_EQ(server.listen(10), 0);
   TCPSocket client(IPV4);
@@ -41,11 +41,11 @@ TEST(Connect, ipv4_copycontructor) {
   client.close();
   server.close();
 
-  server_constructor.reuseAddr(1);
   ASSERT_EQ(server_constructor.bind(1337), 0);
   ASSERT_EQ(server_constructor.listen(10), 0);
-  ASSERT_EQ(client.connect("localhost", 1337), 0);
+  TCPSocket server2(server_constructor);
   server_constructor.close();
+  ASSERT_EQ(client.connect("localhost", 1337), 1);
   client.close();
   server.close();
 
