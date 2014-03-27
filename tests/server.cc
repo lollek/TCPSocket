3825a94b32600a7fc423a76abc17015c982c81e6
Olle K <olle.kvarnstroem@gmail.com>
2014-03-27 20:05:01 +0100

    added test-cases

---
+++ tests/server.cc
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+
+#include "../TCPSocket.hh"
+
+TEST(Constructors, all) {
+  TCPSocket server(IPV4);
+  TCPSocket server2(server);
+  TCPSocket server3 = server2;
+}
+
+TEST(Connect, ipv4) {
+  TCPSocket server(IPV4);
+  server.reuseAddr(1);
+  ASSERT_EQ(server.bind(1337), 0);
+  ASSERT_EQ(server.listen(10), 0);
+  TCPSocket client(IPV4);
+  ASSERT_EQ(client.connect("localhost", 1337), 0);
+  client.close();
+  server.close();
+}
+
+TEST(Connect, ipv6) {
+  TCPSocket server(IPV6);
+  server.reuseAddr(1);
+  ASSERT_EQ(server.bind(1337), 0);
+  ASSERT_EQ(server.listen(10), 0);
+  TCPSocket client(IPV4);
+  ASSERT_EQ(client.connect("localhost", 1337), 0);
+  client.close();
+  server.close();
+}
+
+TEST(Connect, ipv4_copycontructor) {
+  TCPSocket server_constructor(IPV4);
+  TCPSocket server(server_constructor);
+  server.reuseAddr(1);
+  ASSERT_EQ(server.bind(1337), 0);
+  ASSERT_EQ(server.listen(10), 0);
+  TCPSocket client(IPV4);
+  ASSERT_EQ(client.connect("localhost", 1337), 0);
+  client.close();
+  server.close();
+
+  server_constructor.reuseAddr(1);
+  ASSERT_EQ(server_constructor.bind(1337), 0);
+  ASSERT_EQ(server_constructor.listen(10), 0);
+  ASSERT_EQ(client.connect("localhost", 1337), 0);
+  //server = server_constructor;
+  client.close();
+  server_constructor.close();
+
+
+}
