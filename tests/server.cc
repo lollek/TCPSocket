d38373af228bca801efa4d4dedd56aa61a597614
Olle K <olle.kvarnstroem@gmail.com>
2014-03-28 08:30:23 +0100

    added some small tests

--- tests/server.cc
+++ tests/server.cc
@@ -7,26 +7,50 @@
 using namespace std;
 
 TEST(Constructors, all) {
-  TCPSocket server41(IPV4);
-  ASSERT_EQ(server41.getLastError(), string());
+  TCPSocket server(IPV4);
+  ASSERT_EQ(server.getLastError(), string());
+  server.close();
+  server.close();
 
-  TCPSocket server61(IPV6);
-  ASSERT_EQ(server61.getLastError(), string());
+  ASSERT_EQ(server.bind(1337), 0);
+  server.close();
+  server.close();
 
-  TCPSocket server42(server41);
-  ASSERT_EQ(server42.getLastError(), string());
+  ASSERT_EQ(server.bind(1337), 0);
+  ASSERT_EQ(server.listen(10), 0);
+  server.close();
+  server.close();
 
-  TCPSocket server62(server61);
-  ASSERT_EQ(server62.getLastError(), string());
+  TCPSocket server2(server);
+  ASSERT_EQ(server2.getLastError(), string());
+  server2.close();
+  server2.close();
 
-  TCPSocket server43 = server42;
-  ASSERT_EQ(server43.getLastError(), string());
+  ASSERT_EQ(server2.bind(1337), 0);
+  server2.close();
+  server2.close();
 
-  TCPSocket server63 = server62;
-  ASSERT_EQ(server63.getLastError(), string());
+  ASSERT_EQ(server2.bind(1337), 0);
+  ASSERT_EQ(server2.listen(10), 0);
+  server2.close();
+  server2.close();
+
+  TCPSocket server3 = server2;
+  ASSERT_EQ(server3.getLastError(), string());
+  server3.close();
+  server3.close();
+
+  ASSERT_EQ(server3.bind(1337), 0);
+  server3.close();
+  server3.close();
+
+  ASSERT_EQ(server3.bind(1337), 0);
+  ASSERT_EQ(server3.listen(10), 0);
+  server3.close();
+  server3.close();
 }
 
-TEST(Bind, ipv4) {
+TEST(Bind, all) {
   TCPSocket server(IPV4);
   server.reuseAddr(1);
 
@@ -45,83 +69,43 @@ TEST(Bind, ipv4) {
   ASSERT_EQ(aux_server.bind(1337), 1);
   ASSERT_EQ(aux_server.getLastError(), "bind: Address already in use");
 
-  server.close();
-}
+  ASSERT_EQ(aux_server.listen(10), 1);
+  ASSERT_EQ(aux_server.getLastError(), "listen: Bad file descriptor");
 
-TEST(Bind, ipv6) {
-  TCPSocket server(IPV6);
-  server.reuseAddr(1);
-
-  ASSERT_EQ(server.bind(1337), 0);
-  server.close();
-
-  ASSERT_EQ(server.bind(13337), 0);
-  server.close();
-
-  ASSERT_EQ(server.bind(1), 1);
-  ASSERT_EQ(server.getLastError(), "bind: Permission denied");
-
-  ASSERT_EQ(server.bind(1337), 0);
-
-  TCPSocket aux_server(IPV4);
-  ASSERT_EQ(aux_server.bind(1337), 1);
-  ASSERT_EQ(aux_server.getLastError(), "bind: Address already in use");
+  TCPSocket *accepted = aux_server.accept();
+  ASSERT_EQ(accepted, nullptr);
+  ASSERT_EQ(aux_server.getLastError(), "accept: Bad file descriptor");
 
   server.close();
 }
 
-TEST(Connect, ipv4) {
+TEST(Connect, all) {
+  TCPSocket client(IPV4);
+  ASSERT_EQ(client.connect("localhost", 1337), 1);
+  ASSERT_EQ(client.getLastError(), "connect: Connection refused");
+
   TCPSocket server(IPV4);
   server.reuseAddr(1);
   ASSERT_EQ(server.bind(1337), 0);
   ASSERT_EQ(server.listen(10), 0);
-  TCPSocket client(IPV4);
   ASSERT_EQ(client.connect("localhost", 1337), 0);
+
   client.close();
   server.close();
 }
 
-TEST(Connect, ipv6) {
-  TCPSocket server(IPV6);
+TEST(Connect, copyconstructor) {
+  TCPSocket aux_client(IPV4);
+  TCPSocket client(aux_client);
+  ASSERT_EQ(client.connect("localhost", 1337), 1);
+  ASSERT_EQ(client.getLastError(), "connect: Connection refused");
+
+  TCPSocket server(IPV4);
   server.reuseAddr(1);
   ASSERT_EQ(server.bind(1337), 0);
   ASSERT_EQ(server.listen(10), 0);
-  TCPSocket client(IPV4);
   ASSERT_EQ(client.connect("localhost", 1337), 0);
+
   client.close();
   server.close();
-}
-
-TEST(FailedConnect, ipv4) {
-  TCPSocket client(IPV4);
-  ASSERT_EQ(client.connect("localhost", 1337), 1);
-  client.close();
-}
-
-TEST(FailedConnect, ipv6) {
-  TCPSocket client(IPV6);
-  ASSERT_EQ(client.connect("localhost", 1337), 1);
-  client.close();
-}
-
-TEST(Connect, ipv4_copycontructor) {
-  TCPSocket server_constructor(IPV4);
-  server_constructor.reuseAddr(1);
-  TCPSocket server(server_constructor);
-  ASSERT_EQ(server.bind(1337), 0);
-  ASSERT_EQ(server.listen(10), 0);
-  TCPSocket client(IPV4);
-  ASSERT_EQ(client.connect("localhost", 1337), 0);
-  client.close();
-  server.close();
-
-  ASSERT_EQ(server_constructor.bind(1337), 0);
-  ASSERT_EQ(server_constructor.listen(10), 0);
-  TCPSocket server2(server_constructor);
-  server_constructor.close();
-  ASSERT_EQ(client.connect("localhost", 1337), 1);
-  client.close();
-  server.close();
-
-
 }
